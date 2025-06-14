344cbf24325fb403e591a0624f16e4871ec463e7
Laurin Koller <laurin.koller@gmail.com>
2025-06-14 13:35:08 +0200

    Update utils
    add functions to load the datasets

--- utils.py
+++ utils.py
@@ -4,8 +4,28 @@ from pathlib import Path
 
 # %%
 
-def test() -> None:
-    print('hello')
+def make_decoder(feature_description):
+    def decoder(example):
+        example = tf.io.parse_single_example(example, feature_description)
+        features = tf.io.parse_tensor(example['features'], tf.float32)
+        features = tf.reshape(features, [28])
+        label = example['label']
+        return features, label
+    return decoder
+
+def load_dataset(filenames, decoder, ordered=False):
+    AUTO = tf.data.experimental.AUTOTUNE
+    ignore_order = tf.data.Options()
+    if not ordered:
+        ignore_order.experimental_deterministic = False
+    dataset = (
+        tf.data
+        .TFRecordDataset(filenames, num_parallel_reads=AUTO)
+        .with_options(ignore_order)
+        .map(decoder, AUTO)
+    )
+    return dataset
+
 
 
 
