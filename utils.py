c3391f2fa16a4c82bd553b249850a1c525a610f8
Laurin Koller <laurin.koller@gmail.com>
2025-06-16 11:58:58 +0200

    Update utils.py
    add early stopping function

--- utils.py
+++ utils.py
@@ -1,6 +1,7 @@
 import tensorflow as tf, numpy as np, pandas as pd
 import os, sys
 from pathlib import Path
+import torch
 
 # %%
 
@@ -25,6 +26,43 @@ def load_dataset(filenames, decoder, ordered=False):
         .map(decoder, AUTO)
     )
     return dataset
+
+class EarlyStopping:
+    """Stops training when a monitored metric has stopped improving."""
+    def __init__(self, patience=7, min_delta=0, path='checkpoint.pt'):
+        """
+        Args:
+            patience (int): How long to wait after last time the monitored metric improved.
+                            Default: 7
+            min_delta (float): Minimum change in the monitored metric to qualify as an improvement.
+                               Default: 0
+            path (str): Path to save the best model file.
+                        Default: 'checkpoint.pt'
+        """
+        self.patience = patience
+        self.min_delta = min_delta
+        self.path = str(Path(__file__).resolve().parent) + '\\EarlyStopping model\\' + path
+        self.counter = 0
+        self.best_loss = None
+        self.early_stop = False
+
+    def __call__(self, val_loss, model):
+        if self.best_loss is None:
+            self.best_loss = val_loss
+            self.save_checkpoint(val_loss, model)
+        elif val_loss < self.best_loss - self.min_delta:
+            self.best_loss = val_loss
+            self.save_checkpoint(val_loss, model)
+            self.counter = 0
+        else:
+            self.counter += 1
+            if self.counter >= self.patience:
+                self.early_stop = True
+
+    def save_checkpoint(self, val_loss, model):
+        '''Saves model when validation loss decrease.'''
+        #torch.save(model.state_dict(), self.path)
+        return
 
 
 
