e5f718ba5ad3d0d2f74db7a24a757fd0126364a7
Laurin Koller <laurin.koller@gmail.com>
2025-06-16 18:29:18 +0200

    Update main

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from pathlib import Path
 import importlib.util
 import matplotlib.pyplot as plt
 from sklearn.metrics import roc_auc_score
+import time
 
 os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '1' 
@@ -23,6 +24,7 @@ AUTO = tf.data.experimental.AUTOTUNE
 SEED = 0
 torch.manual_seed(SEED)
 np.random.seed(SEED)
+tf.random.set_seed(SEED)
 
 torch.set_default_device("cuda" if torch.cuda.is_available() else "cpu")
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -39,10 +41,14 @@ decoder = u.make_decoder(feature_description)
 train_files = tf.io.gfile.glob(data_dir + '\\training' + '\\*.tfrecord')#[:1]
 valid_files = tf.io.gfile.glob(data_dir + '\\validation' + '\\*.tfrecord')#[:1]
 
-dataset_size = int(11e6)
+# Count the number of samples in the train and validation datasets
+# This takes a long time, so this was run once and it is not manually defined below
+#training_size = u.count_samples(train_files)
+#validation_size = u.count_samples(valid_files)
+
+training_size = int(1.05e7)
 validation_size = int(5e5)
 BATCH_SIZE_PER_REPLICA = 2 ** 11
-training_size = dataset_size - validation_size
 batch_size = BATCH_SIZE_PER_REPLICA
 steps_per_epoch = training_size // batch_size
 validation_steps = validation_size // batch_size
@@ -73,31 +79,17 @@ ds_valid = (
 ds_valid_np = ds_valid.as_numpy_iterator()
 
 # %%
-
-class DenseBlock(nn.Module):
-    def __init__(self, input_size, output_size, activation, dropout_rate=0.1):
-        super().__init__()
-        self.stack = nn.Sequential(
-            nn.Linear(input_size, output_size),
-            nn.BatchNorm1d(num_features=output_size),
-            activation,
-            nn.Dropout(p=dropout_rate)
-        )
-        
-    def forward(self, x):
-        logits = self.stack(x)
-        return logits
     
 class NeuralNetwork(nn.Module):
     def __init__(self):
         super().__init__()
         self.linear_stack = nn.Sequential(
-            DenseBlock(28, 56, nn.Tanh(), 0.1),
-            DenseBlock(56, 56, nn.Tanh(), 0.1),
-            DenseBlock(56, 56, nn.Tanh(), 0.1),
-            DenseBlock(56, 56, nn.Tanh(), 0.1),
-            DenseBlock(56, 56, nn.Tanh(), 0.0),
-            DenseBlock(56, 56, nn.Tanh(), 0.0),
+            u.DenseBlock(28, 56, nn.Tanh(), 0.1),
+            u.DenseBlock(56, 56, nn.Tanh(), 0.1),
+            u.DenseBlock(56, 56, nn.Tanh(), 0.1),
+            u.DenseBlock(56, 56, nn.Tanh(), 0.1),
+            u.DenseBlock(56, 56, nn.Tanh(), 0.0),
+            u.DenseBlock(56, 56, nn.Tanh(), 0.0),
             nn.Linear(56, 1),
             nn.Sigmoid()
         )
@@ -110,12 +102,12 @@ class NeuralNetworkBig(nn.Module):
     def __init__(self):
         super().__init__()
         self.linear_stack = nn.Sequential(
-            DenseBlock(28, 112, nn.Tanh(), 0.1),
-            DenseBlock(112, 112, nn.Tanh(), 0.1),
-            DenseBlock(112, 112, nn.Tanh(), 0.1),
-            DenseBlock(112, 112, nn.Tanh(), 0.1),
-            DenseBlock(112, 112, nn.Tanh(), 0.0),
-            DenseBlock(112, 112, nn.Tanh(), 0.0),
+            u.DenseBlock(28, 112, nn.Tanh(), 0.1),
+            u.DenseBlock(112, 112, nn.Tanh(), 0.1),
+            u.DenseBlock(112, 112, nn.Tanh(), 0.1),
+            u.DenseBlock(112, 112, nn.Tanh(), 0.1),
+            u.DenseBlock(112, 112, nn.Tanh(), 0.0),
+            u.DenseBlock(112, 112, nn.Tanh(), 0.0),
             nn.Linear(112, 1),
             nn.Sigmoid()
         )
@@ -128,8 +120,7 @@ class NeuralNetworkWide(nn.Module):
     def __init__(self, p=0.1):
         super().__init__()
         self.linear_stack = nn.Sequential(
-            DenseBlock(28, 2**11, nn.Tanh(), p),
-            nn.Linear(2**11, 1),
+            nn.Linear(28, 1),
             nn.Sigmoid()
         )
 
@@ -142,17 +133,16 @@ model = NeuralNetworkWide(p=0.1)
 # %%
 
 class Deep(nn.Module):
-    def __init__(self, p=0.1):
+    def __init__(self, units=28, p=0.1):
         super().__init__()
         self.linear_stack = nn.Sequential(
-            DenseBlock(28, 56, nn.Tanh(), p),
-            DenseBlock(56, 56, nn.Tanh(), p),
-            DenseBlock(56, 56, nn.Tanh(), p),
-            DenseBlock(56, 56, nn.Tanh(), p),
-            DenseBlock(56, 56, nn.Tanh(), p),
-            DenseBlock(56, 56, nn.Tanh(), p),
-            nn.Linear(56, 1),
-            nn.Sigmoid()
+            u.DenseBlock(28, units, nn.ReLU(), p),
+            u.DenseBlock(units, units, nn.ReLU(), p),
+            u.DenseBlock(units, units, nn.ReLU(), p),
+            u.DenseBlock(units, units, nn.ReLU(), p),
+            u.DenseBlock(units, units, nn.ReLU(), p),
+            u.DenseBlock(units, units, nn.Tanh(), p),
+            nn.Linear(units, 1)
         )
 
     def forward(self, x):
@@ -160,12 +150,10 @@ class Deep(nn.Module):
         return logits
     
 class Wide(nn.Module):
-    def __init__(self, p=0.1):
+    def __init__(self):
         super().__init__()
         self.linear_stack = nn.Sequential(
-            DenseBlock(28, 2**11, nn.Tanh(), p),
-            nn.Linear(2**11, 1),
-            nn.Sigmoid()
+            nn.Linear(28, 1),
         )
 
     def forward(self, x):
@@ -183,10 +171,11 @@ class DeepWide(nn.Module):
         deep_logits = self.deep(x)
         wide_logits = self.wide(x)
         logits = self.deep_ratio * deep_logits + (1 - self.deep_ratio) * wide_logits
+        logits = torch.sigmoid(logits)
         return logits
 
-deep = Deep(p=0.1)
-wide = Wide(p=0.2)
+deep = Deep(units=2**8, p=0.1)
+wide = Wide()
 model = DeepWide(deep, wide, deep_ratio=0.5)
 
 # %%
@@ -195,9 +184,9 @@ model.to(device)
 
 optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
 lr_scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(
-    optimizer, mode='min', factor=0.2, patience=0, threshold=0.0001, cooldown=0, min_lr=0.0001, eps=1e-08)
+    optimizer, mode='min', factor=0.2, patience=1, threshold=0.0001, cooldown=0, min_lr=0.0001, eps=1e-08)
 loss_fn = nn.BCEWithLogitsLoss()
-early_stopping = u.EarlyStopping(patience=2, min_delta=0.001, path='best_model.pth')
+early_stopping = u.EarlyStopping(patience=5, min_delta=0.000, path='best_model.pth')
 
 # %%
 
@@ -218,8 +207,8 @@ def train_loop(data, model, loss_fn, optimizer):
         if train_step == steps_per_epoch:
             break
         
-        features = torch.from_numpy(features).to(device)
-        labels = torch.from_numpy(labels).to(device)
+        features = torch.from_numpy(copy.copy(features)).to(device)
+        labels = torch.from_numpy(copy.copy(labels)).to(device)
         
         # Compute prediction and loss
         optimizer.zero_grad()
@@ -235,8 +224,8 @@ def train_loop(data, model, loss_fn, optimizer):
 
         if train_step % 10000 == 0:
             loss = loss.item()
-            print(f"loss: {loss:4f}")
-            print(f'Auc: {aucs[-1]:4f}')
+            print(f"loss: {loss:.5f}")
+            print(f'Auc: {aucs[-1]:.5f}')
             
     return losses, aucs
             
@@ -256,8 +245,8 @@ def valid_loop(data, model, loss_fn):
             if valid_step == validation_steps:
                 break
             
-            features = torch.from_numpy(features).to(device)
-            labels = torch.from_numpy(labels).to(device)
+            features = torch.from_numpy(copy.copy(features)).to(device)
+            labels = torch.from_numpy(copy.copy(labels)).to(device)
             
             outputs = model(features)
             outputs = torch.squeeze(outputs)
@@ -271,20 +260,29 @@ def valid_loop(data, model, loss_fn):
         avg_loss = sum_loss / max(sum_count, 1)
         auc = roc_auc_score(val_labels, val_preds)
         
-    print(f"Validation average loss: {avg_loss:4f}")
-    print(f'Validation auc: {auc:4f}\n')
+    print(f"Validation average loss: {avg_loss:.5f}")
+    print(f'Validation auc: {auc:.5f}')
     
     return avg_loss, auc
 
 # %%
 
 epochs = 10
+total_start = time.time()
+
 for t in range(epochs):
     print(f"Epoch {t+1}\n-------------------------------")
     curr_lr = optimizer.param_groups[0]['lr']
-    print(f'Current learning rate: {curr_lr}')
+    #print(f'Current learning rate: {curr_lr}')
+    
+    start_time = time.time()
+    
     train_losses, train_aucs = train_loop(ds_train_np, model, loss_fn, optimizer)
     valid_loss, valid_auc = valid_loop(ds_valid_np, model, loss_fn)
+    
+    duration = time.time()-start_time
+    print(f'Epoch {t+1} finished with duration {duration:.2f}s  and learning rate {curr_lr:.4f} \n')
+    
     train_history.extend(train_losses)
     valid_history.append(valid_loss)
     train_history_auc.extend(train_aucs)
@@ -294,7 +292,9 @@ for t in range(epochs):
         print('Early stopping triggered')
         break
     lr_scheduler.step(valid_history[-1])
-print("Done!")
+    
+total_duration = time.time() - total_start
+print(f"Done! Total elapsed time is {total_duration:.2f}s.")
 
 # %%
 
@@ -303,8 +303,10 @@ best_model.load_state_dict(torch.load(data_dir + '\\EarlyStopping model\\best_mo
 
 # %%
 
-x_train = np.linspace(0, epochs-1, len(train_history))
-x_valid = np.linspace(0, epochs-1, epochs)
+total_epochs = len(valid_history)
+
+x_train = np.linspace(0, total_epochs-1, len(train_history))
+x_valid = np.linspace(0, total_epochs-1, total_epochs)
 
 plt.figure(figsize=(15,8))
 
@@ -320,8 +322,8 @@ n = 300
 
 train_history_truncated = np.array(train_history[:(len(train_history) - (len(train_history) % n))]).reshape(-1, n).mean(axis=1)
 
-x_train = np.linspace(0, epochs-1, len(train_history_truncated))
-x_valid = np.linspace(0, epochs-1, epochs)
+x_train = np.linspace(0, total_epochs-1, len(train_history_truncated))
+x_valid = np.linspace(0, total_epochs-1, total_epochs)
 
 plt.figure(figsize=(15,8))
 
@@ -333,8 +335,8 @@ plt.show()
 
 # %%
 
-x_train = np.linspace(0, epochs-1, len(train_history_auc))
-x_valid = np.linspace(0, epochs-1, epochs)
+x_train = np.linspace(0, total_epochs-1, len(train_history_auc))
+x_valid = np.linspace(0, total_epochs-1, total_epochs)
 
 plt.figure(figsize=(15,8))
 
@@ -350,8 +352,8 @@ n = 300
 
 train_history_auc_truncated = np.array(train_history_auc[:(len(train_history_auc) - (len(train_history_auc) % n))]).reshape(-1, n).mean(axis=1)
 
-x_train = np.linspace(0, epochs-1, len(train_history_auc_truncated))
-x_valid = np.linspace(0, epochs-1, epochs)
+x_train = np.linspace(0, total_epochs-1, len(train_history_auc_truncated))
+x_valid = np.linspace(0, total_epochs-1, total_epochs)
 
 plt.figure(figsize=(15,8))
 
