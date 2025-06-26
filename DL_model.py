7aee734508821b1fef3486201a4010acb76bedfe
Laurin Koller <laurin.koller@gmail.com>
2025-06-26 17:57:40 +0200

    Update DL_model

--- DL_model.py
+++ DL_model.py
@@ -399,6 +399,8 @@ class Deep(nn.Module):
             u.DenseBlock(units, units, nn.GELU(), p),
             u.DenseBlock(units, units, nn.GELU(), p),
             u.DenseBlock(units, units, nn.GELU(), p),
+            u.DenseBlock(units, units, nn.GELU(), p),
+            u.DenseBlock(units, units, nn.GELU(), p),
             u.DenseBlock(units, units, nn.Tanh(), p),
             nn.Linear(units, 1)
         )
@@ -443,7 +445,7 @@ optimizer = torch.optim.AdamW(model.parameters(), lr=0.001, weight_decay=0.05)
 #optimizer = torch.optim.AdamW(model.parameters(), lr=0.0001, weight_decay=0.1)
 #lr_scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='min', factor=0.2, patience=1, threshold=0.0001, cooldown=0, min_lr=0.000001, eps=1e-08)
 #lr_scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='min', factor=0.2, patience=0, threshold=0.00003, cooldown=0, min_lr=0.000001, eps=1e-08)
-lr_scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, 20, 1e-8, -1)
+lr_scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, 50, 1e-8, -1)
 loss_fn = nn.BCEWithLogitsLoss()
 early_stopping = u.EarlyStopping(patience=5, min_delta=0.000, path='best_model.pth')
 
