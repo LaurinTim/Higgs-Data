ab26af4b734761b4a3701cacd997a36a78cfe3e7
Laurin Koller <laurin.koller@gmail.com>
2025-07-17 15:52:44 +0200

    Update utils.py

--- utils.py
+++ utils.py
@@ -5,6 +5,8 @@ import torch
 from torch import nn
 import matplotlib.pyplot as plt
 import importlib.util
+import json, glob
+from tqdm import tqdm
 
 data_dir = str(Path(__file__).resolve().parent)
 
@@ -22,24 +24,27 @@ def accuracy(corr, pred):
     return 1 - np.sum(np.abs(corr - pred))/len(corr)
 
 def get_HIGGS_labels():
-    train_files = tf.io.gfile.glob(data_dir + '\\HIGGS data\\training' + '\\*.tfrecord')
-    valid_files = tf.io.gfile.glob(data_dir + '\\HIGGS data\\validation' + '\\*.tfrecord')
+    train_files = tf.io.gfile.glob(data_dir + '\\HIGGS\\HIGGS data\\training' + '\\*.tfrecord')
+    valid_files = tf.io.gfile.glob(data_dir + '\\HIGGS\\HIGGS data\\validation' + '\\*.tfrecord')
     
     training_size = int(1.05e7)
     validation_size = int(5e5)
-    batch_size = int(5e5)
+    train_batch_size = int(1.05e7)
+    valid_batch_size = int(5e5)
     
-    total_train_steps = training_size / batch_size
-    total_valid_steps = validation_size / batch_size
+    total_train_steps = training_size / train_batch_size
+    total_valid_steps = validation_size / valid_batch_size
     
-    ds_train = uh.make_ds(train_files, batch=batch_size, shuffle=False)
+    ds_train = uh.make_ds(train_files, batch=train_batch_size, shuffle=False)
     ds_train_np = ds_train.as_numpy_iterator()
 
-    ds_valid = us.make_ds(valid_files, batch=batch_size, shuffle=False)
+    ds_valid = uh.make_ds(valid_files, batch=valid_batch_size, shuffle=False)
     ds_valid_np = ds_valid.as_numpy_iterator()
     
     train_labels = []
     valid_labels = []
+    
+    #print(type(next(iter(ds_train_np))))
     
     for train_step, (features, labels) in enumerate(ds_train_np):
         if train_step == total_train_steps:
@@ -61,30 +66,31 @@ def get_SUSY_labels():
     
     training_size = int(4.5e6)
     validation_size = int(5e5)
-    batch_size = int(5e5)
+    train_batch_size = int(4.5e6)
+    valid_batch_size = int(5e5)
     
-    total_train_steps = training_size / batch_size
-    total_valid_steps = validation_size / batch_size
+    total_train_steps = training_size / train_batch_size
+    total_valid_steps = validation_size / valid_batch_size
     
-    ds_train = uh.make_ds(train_files, batch=batch_size, shuffle=False)
+    ds_train = us.make_ds(train_files, batch=train_batch_size, shuffle=False)
     ds_train_np = ds_train.as_numpy_iterator()
 
-    ds_valid = us.make_ds(valid_files, batch=batch_size, shuffle=False)
+    ds_valid = us.make_ds(valid_files, batch=valid_batch_size, shuffle=False)
     ds_valid_np = ds_valid.as_numpy_iterator()
     
     train_labels = []
     valid_labels = []
     
-    for train_step, (features, labels) in enumerate(ds_train_np):
+    for train_step, (features, labels) in tqdm(enumerate(ds_train_np), total=int(total_train_steps)):
         if train_step == total_train_steps:
             break
         
-        train_labels.append(labels)
+        train_labels.extend(labels)
         
-    for valid_step, (features, labels) in enumerate(ds_valid_np):
+    for valid_step, (features, labels) in tqdm(enumerate(ds_valid_np), total=int(total_valid_steps)):
         if valid_step == total_valid_steps:
             break
         
-        valid_labels.append(labels)
+        valid_labels.extend(labels)
     
     return train_labels, valid_labels
