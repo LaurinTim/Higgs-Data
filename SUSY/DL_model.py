1683d9438705b47c7879386632623d55f2aa5256
Laurin Koller <laurin.koller@gmail.com>
2025-07-18 16:02:26 +0200

    New SUSY DL prediction
    New SUSY DL prediction with the parameters: 13 GELU layers, 1 Tanh layer, 2**8 units, lr=1e-3 decrease via Cosine Annealing to 1e-7 by epoch 100.
    The run lasterd 95 epochs, validation loss: 0.420900, validation score: 0.87981, train avg loss: 0.42122, train score: 0.87941.

--- SUSY/DL_model.py
+++ SUSY/DL_model.py
@@ -66,7 +66,7 @@ ds_train_np = ds_train.as_numpy_iterator()
 ds_valid = u.make_ds(valid_files, batch=batch_size, shuffle=False)
 ds_valid_np = ds_valid.as_numpy_iterator()
 
-ds_train_all = u.make_ds(train_files, batch=training_size, shuffle=False)
+ds_train_all = u.make_ds(train_files, batch=50000, shuffle=False)
 ds_train_all_np = ds_train_all.as_numpy_iterator()
 
 ds_valid_all = u.make_ds(valid_files, batch=validation_size, shuffle=False)
@@ -244,7 +244,7 @@ optimizer = torch.optim.AdamW(model.parameters(), lr=1e-3, weight_decay=0.01)
 #lr_scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='min', factor=0.2, patience=1, threshold=0.0001, cooldown=0, min_lr=0.000001, eps=1e-08)
 #lr_scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='min', factor=0.2, patience=0, threshold=0.00003, cooldown=0, min_lr=0.000001, eps=1e-08)
 #lr_scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, 100, 1e-7, -1)
-lr_scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, 50, 1e-7, -1)
+lr_scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, 100, 1e-7, -1)
 loss_fn = nn.BCEWithLogitsLoss()
 early_stopping = u.EarlyStopping(patience=10, min_delta=0.000, path='best_model.pth')
 
@@ -327,6 +327,42 @@ def valid_loop(data, model, loss_fn):
     print(f'Validation auc: {auc:.5f}')
     
     return avg_loss, auc
+
+def get_prediction_train(data, model, loss_fn):
+    # Set the model to evaluation mode - important for batch normalization and dropout layers
+    # Unnecessary in this situation but added for best practices
+    model.eval()
+    sum_loss = 0
+    sum_count = 0
+    ret_labels = []
+    ret_preds = []
+    
+    # Evaluating the model with torch.no_grad() ensures that no gradients are computed during test mode
+    # also serves to reduce unnecessary gradient computations and memory usage for tensors with requires_grad=True
+    with torch.no_grad():
+        for train_step, (features, labels) in enumerate(data):
+            if train_step == training_size/50000:
+                break
+            
+            features = torch.from_numpy(copy.copy(features)).to(device)
+            labels = torch.from_numpy(copy.copy(labels)).to(device)
+            
+            outputs = model(features)
+            outputs = torch.squeeze(outputs)
+            loss = loss_fn(outputs, labels.float()).item()
+            sum_loss += loss
+            sum_count += 1
+            
+            ret_labels.extend(labels.detach().cpu().numpy())
+            ret_preds.extend(outputs.detach().cpu().numpy())
+            
+    avg_loss = sum_loss / max(sum_count, 1)
+    auc = roc_auc_score(ret_labels, ret_preds)
+        
+    print(f"Train average loss: {avg_loss:.5f}")
+    print(f'Train auc: {auc:.5f}')
+    
+    return ret_labels, ret_preds
 
 def get_prediction(data, model, loss_fn):
     # Set the model to evaluation mode - important for batch normalization and dropout layers
@@ -428,7 +464,19 @@ best_model.load_state_dict(torch.load(data_dir + '\\EarlyStopping model\\best_mo
 val_labels, val_pred = get_prediction(ds_valid_all_np, best_model, loss_fn)
 pred_df = pd.DataFrame(val_pred, columns=['pred']).T
 
-train_labels, train_pred = get_prediction(ds_train_all_np, best_model, loss_fn)
+# %%
+
+train_labels, train_pred = get_prediction_train(ds_train_all_np, best_model, loss_fn)
+pred_train_df = pd.DataFrame(train_pred, columns=['pred'])
+
+pred_train_df1 = pred_train_df[:int(training_size/2)]
+pred_train_df2 = pred_train_df[int(training_size/2):]
+
+# %%
+
+pred_df.to_csv(data_dir + '\\predictions\\DL_prediction.csv', index=False)
+pred_train_df1.to_csv(data_dir + '\\predictions\\DL_prediction_train_part1.csv', index=False)
+pred_train_df2.to_csv(data_dir + '\\predictions\\DL_prediction_train_part2.csv', index=False)
 
 # %%
 
@@ -439,10 +487,6 @@ valid_info = pd.DataFrame([valid_history, valid_history_auc], index=['loss_histo
 
 train_info.to_csv(data_dir + "\\DL info\\train_info.csv", index=False)
 valid_info.to_csv(data_dir + "\\DL info\\valid_info.csv", index=False)
-
-# %%
-
-pred_df.to_csv(data_dir + '\\predictions\\DL_prediction.csv', index=False)
 
 # %%
 
