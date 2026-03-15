e536492e9324d14484a0110b745abe7b28ecca91
Laurin Koller <laurin.koller@gmail.com>
2026-03-15 10:54:24 +0100

    Update HIGGS_utils, SUSY_utils
    Remove EarlyStopping class

--- SUSY/SUSY_utils.py
+++ SUSY/SUSY_utils.py
@@ -9,42 +9,6 @@ import json, glob
 data_dir = str(Path(__file__).resolve().parent)
 
 # %%
-
-class EarlyStopping:
-    """Stops training when a monitored metric has stopped improving."""
-    def __init__(self, patience=7, min_delta=0, path='checkpoint.pt'):
-        """
-        Args:
-            patience (int): How long to wait after last time the monitored metric improved.
-                            Default: 7
-            min_delta (float): Minimum change in the monitored metric to qualify as an improvement.
-                               Default: 0
-            path (str): Path to save the best model file.
-                        Default: 'checkpoint.pt'
-        """
-        self.patience = patience
-        self.min_delta = min_delta
-        self.path = data_dir + '\\EarlyStopping model\\' + path
-        self.counter = 0
-        self.best_loss = None
-        self.early_stop = False
-
-    def __call__(self, val_loss, model):
-        if self.best_loss is None:
-            self.best_loss = val_loss
-            self.save_checkpoint(val_loss, model)
-        elif val_loss < self.best_loss - self.min_delta:
-            self.best_loss = val_loss
-            self.save_checkpoint(val_loss, model)
-            self.counter = 0
-        else:
-            self.counter += 1
-            if self.counter >= self.patience:
-                self.early_stop = True
-
-    def save_checkpoint(self, val_loss, model):
-        '''Saves model when validation loss decrease.'''
-        torch.save(model.state_dict(), self.path)
         
 def count_samples(files):
     '''
