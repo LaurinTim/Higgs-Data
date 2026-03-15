0949fd61840deb511265c1631d266468df7f782b
Laurin Koller <laurin.koller@gmail.com>
2026-03-15 12:23:29 +0100

    Update HIGGS_utils, SUSY_utils
    Delete unneeded functions

--- SUSY/SUSY_utils.py
+++ SUSY/SUSY_utils.py
@@ -9,27 +9,6 @@ import json, glob
 data_dir = str(Path(__file__).resolve().parent)
 
 # %%
-        
-def count_samples(files):
-    '''
-    Get the number of samples in files.
-
-    Parameters
-    ----------
-    files : list
-        List containing the paths to the files in which the samples should 
-        be counted.
-
-    Returns
-    -------
-    n : int
-        Number of samples found in the files.
-
-    '''
-    ds = make_ds(files, shuffle=False).cache()
-    n = sum(1 for _ in ds)   # ~0.5 s per million examples
-    
-    return n
 
 class DenseBlock(nn.Module):
     def __init__(self, input_size, output_size, activation, dropout_rate=0.1):
@@ -77,24 +56,7 @@ def plot_training_info(train_loss, valid_loss, train_auc, valid_auc, n=300) -> N
     
     plt.legend(loc='best')
     plt.show()
-    
-def get_feature_spec():
-    return {
-        "label": tf.io.FixedLenFeature([], tf.int64),
-        **{f"f{i}": tf.io.FixedLenFeature([], tf.float32) for i in range(18)}
-    }
 
-def parse_fn(ex_proto):
-    ex = tf.io.parse_single_example(ex_proto, get_feature_spec())
-    label = ex.pop("label")
-    features = tf.stack([ex[f"f{i}"] for i in range(18)], axis=0)
-    return features, label
-
-def make_ds(files, batch=2**11, shuffle=False):
-    #files = glob.glob(pattern)
-    ds = tf.data.TFRecordDataset(files, compression_type="GZIP")
-    if shuffle: ds = ds.shuffle(1_000_000, reshuffle_each_iteration=True)
-    return ds.map(parse_fn, num_parallel_calls=tf.data.AUTOTUNE).batch(batch).prefetch(tf.data.AUTOTUNE).cache().repeat()
 
 
 
