79402441b8a1c387c7e3b2314981f4ea72e0f263
Laurin Koller <laurin.koller@gmail.com>
2026-03-15 10:02:07 +0100

    Create TrainTest.py in HIGGS/utils directory
    Add file with methods for the train and test loops to HIGGS utils directory

---
+++ HIGGS/utils/TainTest.py
@@ -0,0 +1,187 @@
+import numpy as np, pandas as pd
+import torch, copy
+from sklearn.metrics import roc_auc_score
+
+def train_loop(logger, data, model, loss_fn, optimizer, steps_per_epoch, device, s_tot, b_tot, sys_uncertainty):
+    losses = []
+    aucs = []
+    adss = []
+    
+    # Set the model to training mode - important for batch normalization and dropout layers
+    # Unnecessary in this situation but added for best practices
+    model.train()
+    for train_step, (features, labels) in enumerate(data):
+        if train_step == steps_per_epoch:
+            break
+        
+        features = torch.from_numpy(copy.copy(features)).to(device)
+        labels = torch.from_numpy(copy.copy(labels)).to(device)
+        
+        # Compute prediction and loss
+        optimizer.zero_grad()
+        outputs = model(features)
+        
+        outputs = torch.squeeze(outputs)
+        loss = loss_fn(outputs, labels.float())
+        losses.append(loss.detach().cpu().numpy())
+
+        labels_numpy = labels.detach().cpu().numpy()
+        outputs_numpy = outputs.detach().cpu().numpy()
+
+        aucs.append(roc_auc_score(labels_numpy, outputs_numpy))
+        adss.append(u.find_optimal_asimov_threshold(
+            labels_numpy, outputs_numpy, 
+            weight_s=s_tot, weight_b=b_tot, sys_uncertainty=sys_uncertainty, 
+            b_min=1, ret_full=False))
+
+        # Backpropagation
+        loss.backward()
+        optimizer.step()
+
+        if train_step % 10000 == -1:
+            loss = loss.item()
+            print(f"loss: {loss:.5f}")
+            print(f'Auc: {aucs[-1]:.5f}')
+    
+    ads_sigs = [val["max_significance"] for val in adss]
+    
+    print(f'Training average loss: {sum(losses)/len(losses):.5f}')
+    print(f'Training average auc: {sum(aucs)/len(aucs):.5f}')
+    print(f'Training average ADS: {sum(ads_sigs)/len(ads_sigs):.5f}')
+
+    logger.info('Training average loss: %f', round(sum(losses)/len(losses), 5))
+    logger.info('Training average auc: %f', round(sum(aucs)/len(aucs), 5))
+    logger.info('Training average ADS: %f', round(sum(ads_sigs)/len(ads_sigs), 5))
+        
+    return losses, aucs, ads_sigs
+            
+def valid_loop(data, model, loss_fn, validation_steps, device, s_tot, b_tot, sys_uncertainty):
+    # Set the model to evaluation mode - important for batch normalization and dropout layers
+    # Unnecessary in this situation but added for best practices
+    model.eval()
+    sum_loss = 0
+    sum_count = 0
+    val_labels = []
+    val_preds = []
+    
+    # Evaluating the model with torch.no_grad() ensures that no gradients are computed during test mode
+    # also serves to reduce unnecessary gradient computations and memory usage for tensors with requires_grad=True
+    with torch.no_grad():
+        for valid_step, (features, labels) in enumerate(data):
+            if valid_step == validation_steps:
+                break
+            
+            features = torch.from_numpy(copy.copy(features)).to(device)
+            labels = torch.from_numpy(copy.copy(labels)).to(device)
+            
+            outputs = model(features)
+        
+            outputs = torch.squeeze(outputs)
+            loss = loss_fn(outputs, labels.float()).detach().cpu().numpy()
+            sum_loss += loss
+            sum_count += 1
+
+            val_labels.extend(labels.detach().cpu().numpy())
+            val_preds.extend(outputs.detach().cpu().numpy())
+            
+        avg_loss = sum_loss / max(sum_count, 1)
+        auc = roc_auc_score(val_labels, val_preds)
+
+        ads = u.find_optimal_asimov_threshold(
+            val_labels, val_preds, 
+            weight_s=s_tot, weight_b=b_tot, sys_uncertainty=sys_uncertainty, 
+            b_min=1e-8, ret_full=True)
+        
+    print(f"Validation average loss: {avg_loss:.5f}")
+    print(f'Validation auc: {auc:.5f}')
+    print(f"Validation ADS: {ads["max_significance"]:.5f}")
+    print(f"Validation ADS best threshold: {ads["optimal_threshold"]:.5f}")
+    print(f"Validation ADS Signal Events: {ads["signal_events"]:.4f}")
+    print(f"Validation ADS Background Events: {ads["background_events"]:.2f}")
+
+    logger.info("Validation average loss: %f", round(avg_loss, 5))
+    logger.info('Validation auc: %f', round(auc, 5))
+    logger.info("Validation ADS: %f", round(ads["max_significance"], 5))
+    logger.info("Validation ADS best threshold: %f", round(ads["optimal_threshold"], 5))
+    logger.info("Validation ADS Signal Events: %f", round(ads["signal_events"], 4))
+    logger.info("Validation ADS Background Events: %f", round(ads["background_events"], 2))
+    
+    return np.array(val_labels), np.array(val_preds), avg_loss, auc, ads
+
+def get_prediction_train(data, model, loss_fn, device, training_size):
+    # Set the model to evaluation mode - important for batch normalization and dropout layers
+    # Unnecessary in this situation but added for best practices
+    model.eval()
+    ret_labels = []
+    ret_preds = []
+    
+    # Evaluating the model with torch.no_grad() ensures that no gradients are computed during test mode
+    # also serves to reduce unnecessary gradient computations and memory usage for tensors with requires_grad=True
+    with torch.no_grad():
+        for train_step, (features, labels) in enumerate(data):
+            if train_step == training_size/5e4:
+                break
+            
+            features = torch.from_numpy(copy.copy(features)).to(device)
+            labels = torch.from_numpy(copy.copy(labels)).to(device)
+            
+            outputs = model(features)
+        
+            outputs = torch.squeeze(outputs)
+            
+            ret_labels.extend(labels.detach().cpu().numpy())
+            ret_preds.extend(outputs.detach().cpu().numpy())
+            
+    loss = loss_fn(torch.from_numpy(copy.copy(np.array(ret_preds))).float(), torch.from_numpy(copy.copy(np.array(ret_labels))).float())
+    auc = roc_auc_score(ret_labels, ret_preds)
+        
+    print(f'Train loss: {loss:.5f}')
+    print(f'Train auc: {auc:.5f}')
+    
+    #ret_preds = nn.Sigmoid()(torch.from_numpy(copy.copy(np.array(ret_preds)))).detach().cpu().numpy()
+    
+    return ret_labels, ret_preds
+
+def get_prediction(data, model, loss_fn, device, s_tot, b_tot, sys_uncertainty):
+    # Set the model to evaluation mode - important for batch normalization and dropout layers
+    # Unnecessary in this situation but added for best practices
+    model.eval()
+    sum_loss = 0
+    sum_count = 0
+    val_labels = []
+    val_preds = []
+    val_inputs = []
+    
+    # Evaluating the model with torch.no_grad() ensures that no gradients are computed during test mode
+    # also serves to reduce unnecessary gradient computations and memory usage for tensors with requires_grad=True
+    with torch.no_grad():
+        features, labels = next(iter(data))
+        
+        features = torch.from_numpy(copy.copy(features)).to(device)
+        labels = torch.from_numpy(copy.copy(labels)).to(device)
+        
+        outputs = model(features)
+
+        outputs = torch.squeeze(outputs)
+        loss = loss_fn(outputs, labels.float()).item()
+        sum_loss += loss
+        sum_count += 1
+
+        val_labels.extend(labels.detach().cpu().numpy())
+        val_preds.extend(outputs.detach().cpu().numpy())
+        val_inputs.extend(features.detach().cpu().numpy())
+            
+        avg_loss = sum_loss / max(sum_count, 1)
+        auc = roc_auc_score(val_labels, val_preds)
+        ads = u.find_optimal_asimov_threshold(
+            val_labels, val_preds, 
+            weight_s=s_tot, weight_b=b_tot, sys_uncertainty=sys_uncertainty, 
+            b_min=1e-8, num_thresholds=1001, ret_full=True)
+        
+    print(f"Validation loss: {avg_loss:.6f}")
+    print(f'Validation auc: {auc:.5f}')
+    print(f'Validation ads: {ads["max_significance"]:.5f}')
+    
+    #val_preds = nn.Sigmoid()(torch.from_numpy(copy.copy(np.array(val_preds)))).detach().cpu().numpy()
+    
+    return np.array(val_labels), np.array(val_inputs), np.array(val_preds), ads
