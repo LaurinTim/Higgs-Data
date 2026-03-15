41c8e774e65adf135b9484ffc2f8d0effe3d4340
Laurin Koller <laurin.koller@gmail.com>
2026-03-15 12:20:08 +0100

    Update utils.dataset
    Add count_samples function

--- utils/dataset.py
+++ utils/dataset.py
@@ -1,4 +1,6 @@
 import os
+import copy
+from pathlib import Path
 
 os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '1'
@@ -7,16 +9,16 @@ import tensorflow as tf
 from pathlib import Path 
 
 def get_files(data_dir):
-    train_files = (data_dir / "training").glob("*.tfrecord")
-    val_files = (data_dir / "validation").glob("*.tfrecord")
+    train_files = tf.io.gfile.glob(str(data_dir / 'training' / '*.tfrecord'))[:2]
+    val_files = tf.io.gfile.glob(str(data_dir / 'validation' / '*.tfrecord'))[:2]
     return train_files, val_files
 
 def make_ds(name, batch=2**11, shuffle=False):
     BASE_DIR = Path(__file__).resolve().parent.parent
-    if name == 'HIGGS':
+    if name.lower().strip() == 'higgs':
         from .HIGGS import dataset
         data_dir = BASE_DIR / "data" / "HIGGS data"
-    elif name == 'SUSY':
+    elif name.lower().strip() == 'susy':
         from .SUSY import dataset
         data_dir = BASE_DIR / "data" / "SUSY data"
     else:
@@ -34,23 +36,26 @@ def make_ds(name, batch=2**11, shuffle=False):
 
     return ret_train, ret_val
 
-def count_samples(files):
-    '''
-    Get the number of samples in files.
-
-    Parameters
-    ----------
-    files : list
-        List containing the paths to the files in which the samples should 
-        be counted.
-
-    Returns
-    -------
-    n : int
-        Number of samples found in the files.
-
-    '''
-    ds = make_ds(files, shuffle=False).cache()
-    n = sum(1 for _ in ds)   # ~0.5 s per million examples
+def count_samples(name):
+    BASE_DIR = Path(__file__).resolve().parent.parent
+    if name.lower().strip() == 'higgs':
+        from .HIGGS import dataset
+        data_dir = BASE_DIR / "data" / "HIGGS data"
+    elif name.lower().strip() == 'susy':
+        from .SUSY import dataset
+        data_dir = BASE_DIR / "data" / "SUSY data"
+    else:
+        raise ValueError(f"Unknown dataset name: {name}")
     
-    return n
+    files_train = tf.io.gfile.glob(str(data_dir / 'training' / '*.tfrecord'))[:2]
+    files_val = tf.io.gfile.glob(str(data_dir / 'validation' / '*.tfrecord'))[:2]
+    
+    train_count = 0
+    for filename in files_train:
+        train_count += sum(1 for _ in tf.data.TFRecordDataset(filename, compression_type="GZIP"))
+    
+    val_count = 0
+    for filename in files_val:
+        val_count += sum(1 for _ in tf.data.TFRecordDataset(filename, compression_type="GZIP"))
+    
+    return train_count, val_count
